4725cbc42043e9d97079a743c957c630286b25c4
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-03-24 09:41:54 +0000

    content config done

---
+++ basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict";
+// this treats all JS code as per the new version
+// even if we dont write this it will be considered as the new version 
+
+// alert(3+3)
+// this gives us a pop up in console but we are using node js not browser
+
+// check mdn for documentation
+// official documentation is ECMA
+
+let name="sanya"
+let age=20
+let isloggedin=false
+
+// number-range 2 power 53ish
+// bigint
+// string
+// boolean
+// null->stand alone value, it is empty value or zero
+// undefined->value is not defined
+// symbol->unique 
+
+// object
+
+console.log(typeof null) 
+// this gives object
+
+console.log(typeof undefined)
+// this gives undefined only
+
