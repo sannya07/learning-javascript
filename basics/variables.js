dd2453c198c82ac2c1bd325308e4be92ceb04021
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-03-24 09:05:05 +0000

    comit config done

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail= "sanya@gmail.com"
+var accountPassword= "12345"
+accountcity="Jaipur"
+let accountdata;
+
+// accountId=2
+accountEmail="sera@hc.com"
+accountPassword="121212"
+accountcity="Delhi";
+
+console.log(accountId);
+// we cannot change variables declared using const
+console.table([accountId,accountEmail,accountPassword,accountcity,accountdata])
+
+//we do not use var anymore instead we use let as it overcomes the block scope issue
+
+
