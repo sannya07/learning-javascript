a855c99363d96d68d41e47325c487dd66df561c4
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-03-26 12:04:28 +0000

    content commit done

---
+++ basics/numbersandmath.js
@@ -0,0 +1,9 @@
+const score=400;
+console.log(score);
+// prototype is number
+const balance=new Number(100);
+console.log(balance);
+// converts to strings
+console.log(balance.toString().length);
+// gives percission value eg in this case 100.000
+console.log(balance.toFixed(3));
