b7c5f1cfc5d87a715df1ea36f3dd81af085d5e82
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-03-31 07:41:05 +0000

    container config done

---
+++ functions/func.js
@@ -0,0 +1,19 @@
+function saymyname(){
+    console.log('s');
+    console.log('a');
+    console.log('n');
+    console.log('y');
+    console.log('a');
+}
+saymyname()
+// parameters
+function addtwonumbers(num1,num2){
+    console.log(num1+num2);
+}
+
+// arguments
+const result=addtwonumbers(3,2); 
+// this will give ans
+console.log(result) 
+// this will give undefined
+
