deef157437060ecbb2eac061f3e3de0bcfc94326
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-04-01 08:31:27 +0000

    container config done

--- functions/func.js
+++ functions/func.js
@@ -17,3 +17,27 @@ const result=addtwonumbers(3,2);
 console.log(result) 
 // this will give undefined
 
+function add2numbers(num1,num2){
+    let result=num1+num2
+    return result
+}
+
+console.log(add2numbers(3,2)) 
+
+function loginusermessage(username){
+    if(username === undefined){
+        console.log("please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// if we write return then code stops running
+
+console.log(loginusermessage("sanya"));
+// if we wouldnt have passed any arguments then it will say undefined just logged in
+console.log(loginusermessage());
+
+// undefined is considered false 
+// empty string is also considered false
+
