f314506d9a6358c85eac01cef2c8d63e448e29c8
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-04-01 09:34:10 +0000

    container config done

--- functions/scopes.js
+++ functions/scopes.js
@@ -11,3 +11,28 @@ console.log(c);
 
 // global scope is declared normally
 // block scope is inside a block {}
+
+// nested scope
+function one(){
+    const username="sanya"
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+// child function can access parents function variables but not visa versa
+// same with any nested case example ifs
+
+function addone(num){
+    return num+1
+}
+
+const addtwo=function(num){
+    return num+2
+}
+// this is also called expression
+
+// both are ways to declare functions
