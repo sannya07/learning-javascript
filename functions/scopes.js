da42422e772c7f267ebfd9135c1b6fbc692c07cf
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-04-01 09:24:50 +0000

    container config done

---
+++ functions/scopes.js
@@ -0,0 +1,13 @@
+let a=10
+const b=20
+var c=30
+
+// var gives a problem with scope
+
+console.log(a);
+console.log(b);
+console.log(c);
+// we have block scope and global scope
+
+// global scope is declared normally
+// block scope is inside a block {}
