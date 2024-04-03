3d5295b834b0aafd279c4ad15da0dda3ce2572aa
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-04-03 04:31:04 +0000

    container config done

--- basics/truthy.js
+++ basics/truthy.js
@@ -26,3 +26,25 @@ if(Object.keys(emptyobj).length===0){
 // false=='' is true
 // 0=='' is also true
 
+// NULLISH COALECING OPERATOR (??)
+
+let val1;
+// val1= 5 ?? 10
+val1=null??10
+console.log(val1);
+
+let var1;
+var1= undefined?? 15
+console.log(var1);
+
+let val2;
+val2=null??5??10;
+console.log(val2);
+
+
+
+// ternary 
+// condition? true:false 
+
+const price=100
+price>=80? console.log("good"):console.log("bad");
