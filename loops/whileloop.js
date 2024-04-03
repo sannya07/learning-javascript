7480d171f29f10bec6758f48ff7c3be027178356
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-04-03 05:23:38 +0000

    container config done

---
+++ loops/whileloop.js
@@ -0,0 +1,26 @@
+// for index
+let index=0;
+while(index<=10){
+    console.log(`value of index is ${index}`) ;
+    index=index+2;
+}
+// for array
+let myarray=['flash','batman','spiderman']
+let i=0;
+while(i<myarray.length){
+    console.log(myarray[i]);
+    i++;
+}
+
+// do while loop
+// let score=1;
+// do{
+//     console.log(`score is ${score}`)
+//     score++;
+// }while(score<=5)
+
+let score=6;
+do{
+    console.log(`score is ${score}`)
+    score++;
+}while(score<=5)
