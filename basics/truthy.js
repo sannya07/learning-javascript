a8a2b5f56763e92323afa5a185d2023c7a069e88
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-04-03 04:20:09 +0000

    container config done

---
+++ basics/truthy.js
@@ -0,0 +1,28 @@
+// const useremail ="sanya@gmail.com"
+// // empty array ,"0","false",'false'," ",{},function(){} is truthy
+// // false,0,-0,bigint 0n,empty string,Nan, undefined, null are falsy
+// if(useremail){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("no user email");
+// }
+
+
+// const useremail=[];
+// if(useremail.length===0){
+//     console.log("array is empty")
+// }
+
+
+const emptyobj={}
+
+if(Object.keys(emptyobj).length===0){
+    console.log("object is empty");
+}
+
+
+// false==0 is true
+// false=='' is true
+// 0=='' is also true
+
